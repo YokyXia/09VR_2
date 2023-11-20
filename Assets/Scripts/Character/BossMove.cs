7cef202a9b71ef5c89db0e31977000aa0fd03488
Yoky <2429856747@qq.com>
2023-11-21 04:17:50 +0800

    feat: boss障碍物后移动

--- Assets/Scripts/Character/BossMove.cs
+++ Assets/Scripts/Character/BossMove.cs
@@ -7,7 +7,10 @@ public class BossMove : MonoBehaviour
 
     private int currentWaypointIndex = 0;  // 当前运动点的索引
     private bool movingForward = true;  // 是否正向运动
+    private bool boolflag = true;
     public GameObject boss;
+    private float time = 0f;
+    private int flag = 0;
 
     private void Update()
     {
@@ -31,8 +34,8 @@ public class BossMove : MonoBehaviour
 
         // 若物体已接近当前运动点，则切换到下一个运动点
         if (Vector3.Distance(transform.position, currentWaypoint.position) < 0.01f)
-        {
-            // 切换运动方向
+        {          // 切换运动方向
+          
             if (movingForward)
             {
                 currentWaypointIndex++;
@@ -52,5 +55,47 @@ public class BossMove : MonoBehaviour
                 }
             }
         }
+
+        if(flag!=currentWaypointIndex)
+        {
+            speed = 0f;
+            time += Time.deltaTime;
+            if (time > 1)
+            {
+                flag = currentWaypointIndex;
+                speed = 1f;
+                time = 0f;
+            }
+        }
+        //if (flag == currentWaypointIndex)
+        //{
+        //    speed = 0f;
+        //    time += Time.deltaTime;
+        //    Debug.Log("Time:" + time);
+        //    if (time > 1f)
+        //    {
+        //        if (boolflag)
+        //        {
+        //            flag++;
+        //            speed = 1;
+        //            time = 0f;
+        //            if (flag >= waypoints.Length)
+        //            {
+        //                flag = waypoints.Length - 2;
+        //                boolflag = false;
+        //            }
+        //        }
+        //        else
+        //        {
+        //            flag--;
+        //            speed = 1;
+        //            time = 0f;
+        //            if (flag < 0)
+        //            {
+
+        //            }
+        //        }
+        //    }
+        //}
     }
 }
